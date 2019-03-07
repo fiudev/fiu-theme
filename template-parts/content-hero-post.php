12e0aa51c8615ec39262a7336d6b5e4c0ac5ce75
david-castaneda <dcast188@fiu.edu>
2019-03-07 14:34:21 -0500

    added post hero

--- template-parts/content-hero-post.php
+++ template-parts/content-hero-post.php
@@ -26,7 +26,7 @@
 
 .home-slide .image img {
     object-fit: cover;
-    object-position: top;
+    /* object-position: top; */
     width: inherit;
     height: inherit;
     min-width: 100%;
@@ -61,12 +61,91 @@
     font-size: 30px;
     padding: 5px 10px;
 }
+
+/* Slide Info */
+.slide-info {
+    position: absolute;
+    z-index: 1;
+    width: 60%;
+    align-items: center;
+    display: grid;
+    justify-content: center;
+    margin-top: 150px;
+}
+
+.slide-info h1 {
+    color: white;
+    background-color: rgba(8, 29, 63, 0.5);
+    padding: 0 15px;
+    width: 450px;
+}
+
+.slide-button button {
+    color: #081D3F;
+    background-color: #F8C93D;
+    font-weight: 600;
+    text-decoration: underline;
+}
+
+
+@media (max-width: 1024px) and (min-width: 641px) {
+
+    .home-slide,
+    .slick-hero,
+    .home-slide .image img {
+        height: 600px;
+    }
+
+    /* Slide Info */
+    .slide-info {
+        width: 80%;
+    }
+
+}
+
+@media (max-width: 641px) {
+
+    .home-slide,
+    .slick-hero,
+    .home-slide .image img {
+        height: 450px;
+    }
+
+    /* Slide Info */
+
+    #content {
+        padding: 0px !important;
+    }
+
+    .slide-info {
+        width: 100%;
+    }
+
+    .slide-info h1 {
+        width: 300px;
+    }
+}
 </style>
 
 <div class="slick-hero">
+
+    <div class="slide-info">
+        <div class="slide-title">
+            <h1>Senior Design Showcase Attracts Talent Recruiters</h1>
+        </div>
+
+        <div class="slide-button">
+            <a href="#"><button>Read story</button></a>
+        </div>
+
+    </div>
+
+
+
     <div class="home-slide">
         <div class="image">
-            <img src="https://www.cis.fiu.edu/wp-content/uploads/2017/05/gwc-scis-photo.jpg">
+            <img
+                src="https://cec.fiu.edu/wp-content/uploads/2018/11/ranu-jung-prosthetic-hand-system-fiu-computing-engineering-1600x1068.jpg">
         </div>
         <div class="image">
             <img src="https://www.fiu.edu/_assets/images/academics-banner.jpg">
