c1c2c64bc47362c1117cbcf14a1425bcad10d588
david-castaneda <dcast188@fiu.edu>
2019-03-13 11:27:47 -0400

    added news section

---
+++ template-parts/events/content-three-column.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Template part for displaying newse content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package SCIS_Two
+ */
+
+?>
+<style>
+.news-three-column {
+    padding-left: 0.9375rem;
+    padding-right: 0.9375rem;
+}
+
+.news-three-column .news-home-stacked-title span {
+    margin: 15px 0px;
+}
+
+.news-three-column .news-image-thumb {
+    min-height: 200px;
+}
+</style>
+
+<article>
+    <div class="row">
+        <div class="small-12 columns">
+            <h2 class="bordered-title"><span>Events</span></h2>
+        </div>
+    </div>
+    <div class="row news-container news-three-column" data-equalizer="" data-equalizer-mq="medium-up">
+        <div class="row news-local-wrapper">
+
+            <!-- Start Event Wrapper -->
+            <a href="https://news.fiu.edu/2019/03/everglades-scholar-brings-together-stakeholders-for-common-sense-restoration/131309"
+                target="_blank">
+                <div class="large-4 small-12 medium-4 columns">
+
+                    <!-- Event Image -->
+                    <div class="news-image-thumb"
+                        style="background:url('https://www.cis.fiu.edu/wp-content/uploads/2019/03/ai-digital-sign.png') #F7F7F7">
+                        <div class="news-date-box">Mar<br />06</div>
+                    </div>
+
+
+                    <!-- Event Info -->
+                    <div class="news-home-stacked-title">
+                        <span>ACM INTRO TO AI AND ML WORKSHOP</span>
+                        <p>Spring 2019 Senior Design Project Showcase will be held on Thursday, April 18, 2019, at
+                            the FIU Ocean Bank Convocation Center. The event will bring several undergraduate
+                            students from different [&#8230;]
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <!-- End Event Wrapper -->
+
+            <!-- Start Event Wrapper -->
+            <a href="https://news.fiu.edu/2019/03/everglades-scholar-brings-together-stakeholders-for-common-sense-restoration/131309"
+                target="_blank">
+                <div class="large-4 small-12 medium-4 columns">
+
+                    <!-- Event Image -->
+                    <div class="news-image-thumb"
+                        style="background:url('https://www.cis.fiu.edu/wp-content/uploads/2019/03/etiquette_workshop.jpg') #F7F7F7">
+                        <div class="news-date-box">Mar<br />06</div>
+                    </div>
+
+
+                    <!-- Event Info -->
+                    <div class="news-home-stacked-title">
+                        <span>INTERVIEW ETIQUETTE WORKSHOP</span>
+                        <p>Spring 2019 Senior Design Project Showcase will be held on Thursday, April 18, 2019, at
+                            the FIU Ocean Bank Convocation Center. The event will bring several undergraduate
+                            students from different [&#8230;]
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <!-- End Event Wrapper -->
+
+            <!-- Start Event Wrapper -->
+            <a href="https://news.fiu.edu/2019/03/everglades-scholar-brings-together-stakeholders-for-common-sense-restoration/131309"
+                target="_blank">
+                <div class="large-4 small-12 medium-4 columns">
+
+                    <!-- Event Image -->
+                    <div class="news-image-thumb"
+                        style="background:url('https://i0.wp.com/www.cis.fiu.edu/wp-content/uploads/2019/02/GT_DS.png') #F7F7F7">
+                        <div class="news-date-box">Mar<br />06</div>
+                    </div>
+
+
+                    <!-- Event Info -->
+                    <div class="news-home-stacked-title">
+                        <span>UPE’s Spring Smash Tournament</span>
+                        <p>Spring 2019 Senior Design Project Showcase will be held on Thursday, April 18, 2019, at
+                            the FIU Ocean Bank Convocation Center. The event will bring several undergraduate
+                            students from different [&#8230;]
+                        </p>
+                    </div>
+                </div>
+            </a>
+            <!-- End Event Wrapper -->
+
+        </div>
+    </div>
+
+    <!-- Read More button -->
+    <div class="row">
+        <div class="small-8 medium-4 small-centered columns button-container">
+            <a class="fiu-button fiu-button-blue-outline" href="news-and-events/index.html" target="_self">More
+                Events</a>
+        </div>
+    </div>
+
+</article>
---
+++ template-parts/news/content-featured.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Template part for displaying newse content in page.php
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package SCIS_Two
+ */
+
+?>
+<style>
+
+</style>
+
+<article>
+    <div class="row">
+        <div class="small-12 columns">
+            <h2 class="bordered-title"><span>News</span></h2>
+        </div>
+    </div>
+    <div class="row news-container" data-equalizer="" data-equalizer-mq="medium-up">
+        <div class="large-4 medium-12 small-12 columns" data-equalizer-watch=""><a
+                href="https://casenews.fiu.edu/2019/03/11/bsi-faculty-member-teams-up-with-engineering-resources-group/"
+                target="_blank">
+                <div class="news-image-thumb-home news-home-wrapper"
+                    style="background:url('https://www.cis.fiu.edu/wp-content/uploads/2019/01/mlh_red-1.jpg') #F7F7F7">
+                </div>
+            </a></div>
+        <div class="large-3 medium-12 small-12 columns"><a
+                href="https://casenews.fiu.edu/2019/03/11/bsi-faculty-member-teams-up-with-engineering-resources-group/"
+                target="_blank">
+                <div class="news-home-wrapper news-local-wrapper">
+                    <div class="news-home-title"><span>MANGOHACKS</span>
+                        <p>Florida’s Sweetest Hackathon A place for discovery Mangohacks is a 36 hour hackathon that
+                            encourages learning, collaboration, growth, innovation, and fun. We will welcome 500+
+                            students from Florida and across the country[&#8230;]</p>
+                    </div>
+                </div>
+            </a></div>
+        <div class="large-5 columns" data-equalizer-watch="">
+            <div class="row news-local-wrapper"><a
+                    href="https://news.fiu.edu/2019/03/everglades-scholar-brings-together-stakeholders-for-common-sense-restoration/131309"
+                    target="_blank">
+                    <div class="large-5 small-12 medium-4 columns">
+                        <div class="news-image-thumb"
+                            style="background:url('https://www.cis.fiu.edu/wp-content/uploads/2019/02/88816697-471c-435f-89c2-93e0c46674c3.jpg') #F7F7F7">
+                        </div>
+                    </div>
+                    <div class="large-7 small-12 medium-8 columns">
+                        <div class="news-home-stacked-title"><span>FIUCEC SPRING 2019 SENIOR DESIGN PROJECT
+                                SHOWCASE</span>
+                            <p>Spring 2019 Senior Design Project Showcase will be held on Thursday, April 18, 2019, at
+                                the FIU Ocean Bank Convocation Center. [&#8230;]
+                            </p>
+                        </div>
+                    </div>
+                </a></div>
+            <div class="row news-local-wrapper"><a
+                    href="https://casenews.fiu.edu/2019/03/06/seas-student-pays-it-forward/" target="_blank">
+                    <div class="large-5 small-12 medium-4 columns">
+                        <div class="news-image-thumb"
+                            style="background:url('https://www.cis.fiu.edu/wp-content/uploads/2018/11/XirMREcQEa2JMUuxt6DHA.jpg') #F7F7F7">
+
+                        </div>
+                    </div>
+                    <div class="large-7 small-12 medium-8 columns">
+                        <div class="news-home-stacked-title"><span>UPE & WICS AT DOLPHINS STEM DAY</span>
+                            <p>Growing up, William Padua stayed at shelters and faced a lack of sufficient food.
+                                Although Padua faced much adversity, the [&#8230;]</p>
+                        </div>
+                    </div>
+                </a></div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="small-8 medium-4 small-centered columns button-container"><a
+                class="fiu-button fiu-button-blue-outline" href="news-and-events/index.html" target="_self">More
+                News</a></div>
+    </div>
+</article>
